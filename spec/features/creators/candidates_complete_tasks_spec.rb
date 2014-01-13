5da1e7e66fabe773a09722ea597549d9798c42d3
Ezekiel <ezerivadeneiral@gmail.com>
2014-01-13 16:09:49 -0300

    [Training Programs & Tasks #15]
      - Adds new spec
      - Changes invitation response

--- spec/features/creators/candidates_complete_tasks_spec.rb
+++ spec/features/creators/candidates_complete_tasks_spec.rb
@@ -9,6 +9,7 @@ feature "Complete a task" do
     page.driver.post task_complete_path(task)
 
     expect( task.candidates.where(email:candidate.email) ).to exist
+    expect( candidate.tasks.count ).to eql(1)
   end
 
 end
